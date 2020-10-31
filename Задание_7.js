68738ec7361e8c2ad2561f39f22ef304e351d213
jul-mt <hitomi@yandex.ru>
2020-10-31 16:01:19 +0300

    Update Задание_7.js

--- Задание_7.js
+++ Задание_7.js
@@ -1,15 +1,24 @@
 let arr = [1, NaN, 0, "ааа", 2, null, 5];
 let even = 0;
 let odd = 0;
+let zeros = 0;
 for (let i = 0; i < arr.length; i++) { 
-  if ((typeof(arr[i]) !== "number") || (arr[i] === 0) || arr[i]+"" == "NaN") {
+  if ((typeof(arr[i]) !== "number") || arr[i]+"" == "NaN") {
    } else {
-      if (arr[i]%2 === 0) {
-    	even++;
+      if (arr[i] === 0) { 
+       zeros++;
       } else {
-    	odd++;
-      }
+        if (arr[i]%2 === 0) {
+          even++;
+        } else {
+          odd++;
+       }
+      } 
    }
 }
-
-console.log("Четных чисел: "+even+". Нечетных: "+odd);
+  
+if (zeros === 0) {
+  console.log("Четных чисел: "+even+". Нечетных: "+odd);
+} else {
+  console.log("Четных чисел: "+even+". Нечетных: "+odd+". Нулей: "+zeros);
+}  
